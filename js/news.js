d73907e1da5157fdd2505e2d1912bbc9a4c53a04
Jhimi Talukder <talukderjhimi@gmail.com>
2023-03-02 02:50:57 +0600

    showing new

--- js/news.js
+++ js/news.js
@@ -24,7 +24,29 @@ const fetchcategoryid = (category_id, category_name) => {
 
 const itemshow = (data, category_name) => {
    document.getElementById('item-news').innerText = data.length;
-   document.getElementById('new-section').innerText = `${category_name}`
+   document.getElementById('new-section').innerText = `${category_name}`;
+
+
+   let container = document.getElementById('all-news');
+   data.forEach(allnews => {
+    console.log(allnews)
+    container.innerHTML += `
+    <div class="card mb-3" style="width: 100%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${allnews.image_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${allnews.title}</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+    `
+   });
 }
 
 
